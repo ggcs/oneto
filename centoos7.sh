357b8d6a47156ee95282d8b73de964d8fedb7c8d
IloveJFla <32790381+IloveJFla@users.noreply.github.com>
2018-10-31 14:30:00 +0800

    Update centoos7.sh

--- centoos7.sh
+++ centoos7.sh
@@ -1,10 +1,83 @@
-sed -i "s/#Port .*/Port 23456/g" /etc/ssh/sshd_config && systemctl restart sshd.service
-firewall-cmd --permanent --zone=public --add-port=23456/tcp
+#!/bin/bash
+#################################################
+#   CentOS7初始化脚本
+#################################################
+#
+
+Green_font="\033[32m" && Yellow_font="\033[33m" && Red_font="\033[31m" && Font_suffix="\033[0m"
+Info="${Green_font}[Info]${Font_suffix}"
+Error="${Red_font}[Error]${Font_suffix}"
+reboot="${Yellow_font}重启${Font_suffix}"
+echo -e "${Green_font}
+#================================================
+#              CentOS7初始化脚本
+#================================================
+${Font_suffix}"
+
+check_sys(){
+    if [[ -f /etc/redhat-release ]]; then
+        release="centos"
+    elif cat /etc/issue | grep -q -E -i "debian"; then
+        release="debian"
+    elif cat /etc/issue | grep -q -E -i "ubuntu"; then
+        release="ubuntu"
+    elif cat /etc/issue | grep -q -E -i "centos|red hat|redhat"; then
+        release="centos"
+    elif cat /proc/version | grep -q -E -i "debian"; then
+        release="debian"
+    elif cat /proc/version | grep -q -E -i "ubuntu"; then
+        release="ubuntu"
+    elif cat /proc/version | grep -q -E -i "centos|red hat|redhat"; then
+        release="centos"
+    fi
+}
+
+check_system(){
+check_sys
+[[ ${release} != "debian" ]] && [[ ${release} != "ubuntu" ]] && echo -e "${Error} 本脚本不支持当前系统 ${release} !" && exit 1
+}
+
+check_root(){
+	[[ "`id -u`" != "0" ]] && echo -e "${Error} must be root user !" && exit 1
+}
+
+check_kvm(){
+	apt-get update
+	apt-get install -y virt-what
+	apt-get install -y ca-certificates
+	#virt=`virt-what`
+	#[[ "${virt}" = "openvz" ]] && echo -e "${Error} OpenVZ not support !" && exit 1
+	[[ "`virt-what`" != "kvm" ]] && echo -e "${Error} only support KVM !" && exit 1
+}
+
+
+rand(){
+    min=$1
+    max=$(($2-$min+1))
+    num=$(($RANDOM+1000000000)) #增加一个10位的数再求余
+    echo $(($num%$max+$min))
+}
+ 
+
+
+
+install(){
+
+check_system
+check_root
+check_kvm
+
+resize2fs /dev/vda1
+
+rnd=$(rand 40000 50000)
+
+sed -i "s/#Port .*/Port $rnd/g" /etc/ssh/sshd_config && systemctl restart sshd.service
+firewall-cmd --permanent --zone=public --add-port=$rnd/tcp
 firewall-cmd --reload
-firewall-cmd --permanent --query-port=23456/tcp
+firewall-cmd --permanent --query-port=$rnd/tcp
 yum provides semanage
 yum -y install policycoreutils-python
-semanage port -a -t ssh_port_t -p tcp 23456
+semanage port -a -t ssh_port_t -p tcp $rnd
 semanage port -l | grep ssh
 firewall-cmd --permanent --zone=public --remove-port=22/tcp
 firewall-cmd --reload
@@ -66,8 +139,15 @@ rpm --import RPM-GPG-KEY-elrepo.org
 rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-3.el7.elrepo.noarch.rpm
 yum --enablerepo=elrepo-kernel install kernel-ml -y
 grub2-mkconfig -o /boot/grub2/grub.cfg && grub2-set-default 0
-reboot
+echo -e "${Info} 确认内核安装无误后, ${reboot}你的VPS, 开机后再次运行该脚本的第二项！重新连接的端口号为$rnd"
 
+}
+
+
+start(){
+check_system
+check_root
+check_kvm
 uname -r
 cat >>/etc/sysctl.conf << EOF
 net.core.default_qdisc=fq
@@ -75,5 +155,26 @@ net.ipv4.tcp_congestion_control=bbr
 EOF
 sysctl -p
 lsmod | grep bbr
-
 yum clean all
+}
+
+
+echo -e "${Info} 选择你要使用的功能: "
+echo -e "1.初始化\n2.开启BBR算法\n3.设置中文\n4.设置时区"
+read -p "输入数字以选择:" function
+
+while [[ ! "${function}" =~ ^[1-4]$ ]]
+	do
+		echo -e "${Error} 无效输入"
+		echo -e "${Info} 请重新选择" && read -p "输入数字以选择:" function
+	done
+
+if   [[ "${function}" == "1" ]]; then
+	install
+elif [[ "${function}" == "2" ]]; then
+	start
+elif [[ "${function}" == "3" ]]; then
+	status
+else
+	uninstall
+fi
